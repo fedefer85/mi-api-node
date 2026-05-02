7f206edcf9c2dca05bee561dbb9ed0bfd626d837
fedefer85 <fedefer85@hotmail.com>
2026-05-02 15:39:53 -0300

    fixed jwt secret

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ function authMiddleware(req, res, next) {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) || "secreto_local";
     req.user = decoded;
     next();
   } catch {
